30e8b1d99cf5847fb968573898f75d3ba516b06d
Daniel Torres <94501726+DannyTM12@users.noreply.github.com>
2026-07-03 23:46:14 +0000

    Eliminar conversión innecesaria a str en la función addBinary

--- 0067.addBinary.py
+++ 0067.addBinary.py
@@ -1,5 +1,5 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         # Convert binary strings to integers, add them, and convert back to binary string
-        return str(bin(int(a, 2) + int(b, 2))[2:]) 
+        return bin(int(a, 2) + int(b, 2))[2:]
     
